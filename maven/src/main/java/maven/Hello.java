8adca5fe11fc96496f42383dd3820baa24bea45a
Kouissi Mohamed <mohamed.kouissi@gmail.com>
2025-12-15 11:50:48 +0100

    Change greeting message to reflect Monday

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 	public static void main(String[] args)
     {
-        System.out.println("Hello DevOps, Today is Friday afternoon V5...");
+        System.out.println("Hello DevOps, Today is Monday V6...");
     }
 
 }
