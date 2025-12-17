5570d051c75b26c8c5fe4e2d1035532378c1874a
Kouissi Mohamed <mohamed.kouissi@gmail.com>
2025-12-17 16:49:59 +0100

    Update greeting message to version 9

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 	public static void main(String[] args)
     {
-        System.out.println("Hello DevOps, Today is Wednesday afternoon V8...");
+        System.out.println("Hello DevOps, Today is Wednesday afternoon V9...");
     }
 
 }
