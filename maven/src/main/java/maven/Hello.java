9689dc1845c6a5a79c7cf4914230bc0451b04cb2
MOHAMED KOUISSI <mohamed.kouissi@gmail.com>
2025-12-21 13:25:29 +0100

    Revert "update hello java file"
    This reverts commit 1e4eec012b0542d54302d3d8f632d22f287824de.

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 	public static void main(String[] args)
     {
-        System.out.println("Hello DevOps, Today is Sunday afternoon V9...");
+        System.out.println("Hello DevOps, Today is Wednesday afternoon V9...");
     }
 
 }
