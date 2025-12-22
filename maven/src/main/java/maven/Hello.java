dff784947c0d3f28d65f4adfa61e8926c91c912d
Kouissi Mohamed <mohamed.kouissi@gmail.com>
2025-12-22 15:17:15 +0100

    Update Hello.java

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 	public static void main(String[] args)
     {
-        System.out.println("Hello DevOps, Today is Monday 11...");
+        System.out.println("Hello DevOps, Today is Monday aftern V 12...");
     }
 
 }
